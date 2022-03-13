cde4f69b53c390be6bd2e7cab2e7f2c336c91658
kas1976 <kas1976@hotmail.com>
2022-03-13 12:10:25 +0100

    Connectivity testing with GitHub after SSH URL modification.

--- week3test.R
+++ week3test.R
@@ -3,3 +3,5 @@ print("This file was created within RStudio")
 print("And now it lives on GitHub")
 
 print("and Kevin knows this setup is connected.")
+
+print("this change was implemented at 1203 CET by KS to ensure visibility in Github is observed.") 
